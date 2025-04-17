ca20c32306338fa1d24aff9247a3a6896ee4096a
DgLsAlmeida <douglas.alu.lmb@gmail.com>
2025-04-17 18:12:44 -0300

    feat: add semantic release npm to update package json automatically

--- release.config.js
+++ release.config.js
@@ -5,6 +5,7 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/github",
+    "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
